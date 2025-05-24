642c2b49eb57e7d2478dc90c22251f095762286e
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-23 22:50:59 -0400

    Completes lab.

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import time
+def countdown(i=5):
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(i=5):
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+    
